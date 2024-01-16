68b9777efee6e704cbbeea646b1cda8e666f7457
Miqueias dos Santos Sousa Assad <mi.queias2010@hotmail.com>
2024-01-16 11:35:43 -0300

    algumas conveções de código do python pep8

--- pep8.py
+++ pep8.py
@@ -1,5 +1,31 @@
 """
+    1 - a identação no python deve ter 4 espaços
+
+       if(true):
+           print("faça alguma coisa")
+
+    2 - os nomes de função devem inicar em lower case, e caso precise de um nome composto usar
+    underline
+    Padrão: snake_case
+
+    def soma_se():
+        pass
+
+    3 - nome de classe deve iniciar com Maiúscula, e caso tenha nome composto deve usar
+    padrão: PascalCase
+
+    class PessoaJuridica:
+        pass
+"""
+
+"""
+    utilitários do python
+
     dir("miqueias") --> mostra os atributos/propriedades, funções/metodos
 
-    help("miqueias".lower)
+    help("miqueias".lower) --> fornece uma explicação do que o método faz
 """
+
+
+class PessoaJuridica:
+    pass
